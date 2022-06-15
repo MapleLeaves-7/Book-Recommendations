9487daf1aab02ee2541ab1b7c0f275c1f65b26f7
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-15 16:28:53 +0800

    feat: click out of beta website

--- books_scraper/books_scraper/spiders/goodreads.py
+++ books_scraper/books_scraper/spiders/goodreads.py
@@ -1,5 +1,12 @@
 import scrapy
 import re
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class GoodreadsSpider(scrapy.Spider):
@@ -8,25 +15,81 @@ class GoodreadsSpider(scrapy.Spider):
         "https://www.goodreads.com/book/show/22557272-the-girl-on-the-train"]
 
     def __init__(self):
+        self.base_url = "https://www.goodreads.com"
         self.is_beta = False
 
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--incognito')
+        # options.add_argument('--headless')
+        self.driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()), options=options)
+
     def parse(self, response):
-        beta_button = response.xpath('//div[@class="BetaFeedbackButton"]')
+        # Let selenium load the page then pass html to scrapy
+        self.driver.get(response.request.url)
+        sel = scrapy.Selector(text=self.driver.page_source)
+
+        # Check if it is the beta website
+        beta_button = sel.xpath(
+            '//div[@class="BetaFeedbackButton"]')
+
         if beta_button:
-            self.is_beta = True
-            print("Accessing the beta website")
-        else:
-            print("Accessing the regular website")
+            # Click out of the beta website
+            while True:
+                try:
+                    # Find the beta button to click
+                    self.driver.find_element_by_xpath(
+                        '//span[contains(text(),"BETA")]/parent::button').click()
+
+                    leave_beta_button = self.driver.find_element_by_xpath(
+                        '//span[contains(text(),"Leave beta")]/parent::button')
+
+                    # Try until the button loads
+                    while not leave_beta_button:
+                        time.sleep(0.6)
+                        leave_beta_button = self.driver.find_element_by_xpath(
+                            '//span[contains(text(),"Leave beta")]/parent::button')
+
+                    time.sleep(1)
+                    leave_beta_button.click()
+                    break
+                except:
+                    # Reload the page if there is an overlay login window
+                    self.driver.get(response.request.url)
+                    time.sleep(0.6)
+
+            # Wait until the old page has loaded or 30 seconds have passed
+            WebDriverWait(self.driver, 30).until(
+                EC.presence_of_element_located(
+                    (By.CLASS_NAME, "content"))
+            )
+            time.sleep(0.6)
+
+        sel = self.remove_old_modal(sel, response.request.url)
 
         yield {
-            "title": self.get_title(response),
-            "author": self.get_author(response),
-            "description": self.get_description(response),
-            "num_pages": self.get_num_pages(response),
-            "num_ratings": self.get_num_ratings(response),
-            "rating_value": self.get_rating_value(response),
-            "genres": self.get_genres(response)
+            "title": self.get_title(sel),
+            "author": self.get_author(sel),
+            "description": self.get_description(sel),
+            "num_pages": self.get_num_pages(sel),
+            "num_ratings": self.get_num_ratings(sel),
+            "rating_value": self.get_rating_value(sel),
+            "genres": self.get_genres(sel)
         }
+
+    # Check if modal window exists and reload until it is gone
+    def remove_old_modal(self, sel, url):
+        while True:
+            old_modal_window = sel.xpath(
+                '//*[contains(@class,"loginModal")]')
+
+            if not old_modal_window:
+                return sel
+
+            self.driver.get(url)
+            time.sleep(1)
+            sel = scrapy.Selector(text=self.driver.page_source)
 
     def get_title(self, response):
         if self.is_beta:
@@ -89,6 +152,10 @@ class GoodreadsSpider(scrapy.Spider):
 
     def get_genres(self, response):
         if self.is_beta:
+            genre_sections = response.xpath(
+                '//span[@class="BookPageMetadataSection__genre"]')
+            print(genre_sections)
+            genres = "nothing"
             print("haven't handled the beta yet")
         else:
             genre_anchor_tags = response.xpath(
@@ -97,7 +164,7 @@ class GoodreadsSpider(scrapy.Spider):
             for tag in genre_anchor_tags:
                 link = tag.xpath('@href').get()
                 name = tag.xpath('text()').get()
-                genres[link] = name.lower()
+                genres[self.base_url + link] = name.lower()
 
         return genres
 

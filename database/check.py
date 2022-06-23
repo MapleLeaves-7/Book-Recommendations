3cbdc796ae62302dcfcc09dd36085ba0d069672e
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-23 12:25:20 +0800

    fix: check for errors in has_all_data field and update

---
+++ database/check.py
@@ -0,0 +1,34 @@
+# add parent directory to path
+from inspect import getsourcefile
+import os.path
+import sys
+current_path = os.path.abspath(getsourcefile(lambda:0))
+current_dir = os.path.dirname(current_path)
+parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
+
+sys.path.insert(0, parent_dir)
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import and_
+from books_scraper.books_scraper.models import Book, get_engine
+
+engine = get_engine()
+Session = sessionmaker(bind=engine)
+
+def check_has_all_data():
+    with Session.begin() as session:
+        books = session.query(Book).filter(and_(Book.has_all_data == False, Book.has_author == True, Book.has_genre == True, Book.has_setting == True, Book.has_related_books == True, Book.title != None, Book.description != None, Book.num_pages != None, Book.num_ratings != None, Book.rating_value != None, Book.date_published != None))
+        count = 0
+        for book in books:
+            count += 1
+            print(book.title)
+        print(count)
+
+def update_has_all_data():
+    with Session.begin() as session:
+        books = session.query(Book).filter(and_(Book.has_all_data == False, Book.has_author == True, Book.has_genre == True, Book.has_setting == True, Book.has_related_books == True, Book.title != None, Book.description != None, Book.num_pages != None, Book.num_ratings != None, Book.rating_value != None, Book.date_published != None))
+        for book in books:
+            book.has_all_data = True
+
+update_has_all_data()
+check_has_all_data()

d34eca685f49b0bf7ea55e5d80d33cebc0af4e53
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-29 19:04:09 +0800

    feat: use cosine sim from dumped out cosine_sim_matrix

--- database/compare_cosine_sim.py
+++ database/compare_cosine_sim.py
@@ -1,7 +1,4 @@
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
-import time
+import numpy as np
 from random import randint
 import json
 
@@ -12,81 +9,28 @@ from sqlalchemy.orm import sessionmaker
 engine = get_engine()
 Session = sessionmaker(bind=engine)
 
-
-def get_linear_kernel(tfidf_matrix):
-    # Record start time
-    start = time.time()
-
-    # Compute cosine similarity matrix
-    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-
-    # Print cosine similarity matrix
-    print(cosine_sim)
-
-    # Print time taken
-    print("Time taken: %s seconds" % (time.time() - start))
-    return cosine_sim
-
-
-def get_cosine_sim(tfidf_matrix):
-    # Record start time
-    start = time.time()
-
-    # Compute cosine similarity matrix
-    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
-
-    # Print cosine similarity matrix
-    print(cosine_sim)
-
-    # Print time taken
-    print("Time taken: %s seconds" % (time.time() - start))
-    return cosine_sim
-
-
-def get_pd_id(df, db_id):
-    row = df.loc[df['id'] == db_id]
-    if row.empty:
-        return None
-    return row.index.item()
-
-
-def get_db_id(df, pd_id):
-    return df.iloc[[pd_id]]['id'].item()
-
-
 with Session.begin() as session:
-    df = pd.read_sql(session.query(Book).filter(Book.description != None).limit(200).statement, session.bind)
-    description = df['description']
-    all_book_ids = df['id'].to_numpy()
-    # Create TfidfVectorizer object
-    vectorizer = TfidfVectorizer()
-
-    # Generate matrix of word vectors
-    tfidf_matrix = vectorizer.fit_transform(description)
-
-    cosine_sim_matrix = get_linear_kernel(tfidf_matrix)
-    # get_cosine_sim(tfidf_matrix)
+    cosine_sim_matrix = np.load('cosine_sim_matrix.npy')
     books_tfidf = []
 
-    for idx1, row in enumerate(cosine_sim_matrix):
+    for np_id1, row in enumerate(cosine_sim_matrix):
+        book1 = session.query(Book).filter(Book.np_id == np_id1).first()
         tfidf_comparision = {
-            "id": get_db_id(df, idx1),
+            "id": book1.id,
+            "title": book1.title,
             "related": [],
             "unrelated": []
         }
-        book1_id = tfidf_comparision["id"]
-        book1 = session.query(Book).filter(Book.id == book1_id).first()
-        tfidf_comparision["title"] = book1.title
-        related_book_pd_ids = [get_pd_id(df, book.id) for book in book1.related_books]
 
-        for idx2 in related_book_pd_ids:
-            if idx2 is None:
+        related_book_np_ids = [book.np_id for book in book1.related_books]
+
+        for np_id2 in related_book_np_ids:
+            if np_id2 is None:
                 continue
-            sim = cosine_sim_matrix[idx1][idx2]
+            sim = cosine_sim_matrix[np_id1][np_id2]
             sim_python = sim.item()
-            db_id = get_db_id(df, idx2)
-            book = session.query(Book).filter(Book.id == db_id).first()
-            tfidf_comparision["related"].append({"id": db_id,
+            book = session.query(Book).filter(Book.np_id == np_id2).first()
+            tfidf_comparision["related"].append({"id": book.id,
                                                  "title": book.title,
                                                  "sim": sim_python})
 
@@ -94,20 +38,19 @@ with Session.begin() as session:
 
         while len(random_integers) < 5:
             random_num = randint(0, len(cosine_sim_matrix) - 1)
-            if random_num in related_book_pd_ids or random_num == idx1:
+            if random_num in related_book_np_ids or random_num == np_id1:
                 continue
             random_integers.append(random_num)
 
-        for idx2 in random_integers:
-            sim = cosine_sim_matrix[idx1][idx2]
+        for np_id2 in random_integers:
+            sim = cosine_sim_matrix[np_id1][np_id2]
             sim_python = sim.item()
-            db_id = get_db_id(df, idx2)
-            book = session.query(Book).filter(Book.id == db_id).first()
-            tfidf_comparision["unrelated"].append({"id": db_id,
+            book = session.query(Book).filter(Book.np_id == np_id2).first()
+            tfidf_comparision["unrelated"].append({"id": book.id,
                                                    "title": book.title,
                                                    "sim": sim_python})
 
         books_tfidf.append(tfidf_comparision)
 
-    with open('tfidf_comparision.json', 'w', encoding='utf-8') as f:
+    with open('tfidf_comparision1.json', 'w', encoding='utf-8') as f:
         json.dump(books_tfidf, f, ensure_ascii=False, indent=4)

94cc25a7c6daf2ea73507efdf0989ed356eb05bf
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-06-30 17:45:56 +0800

    feat: stem words in description before tokenizing

--- database/save_cosine_sim_matrix.py
+++ database/save_cosine_sim_matrix.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import numpy as np
+import time
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
-import time
 
 from models import Book, get_engine, create_all_tables
 from sqlalchemy.orm import sessionmaker
+
+# get parent directory
+import os
+path = os.getcwd()
+parent_dir = os.path.abspath(os.path.join(path, os.pardir))
+
+import nltk
+nltk.data.path.append(parent_dir + "/nltk_data")
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize 
 
 
 engine = get_engine()
@@ -44,13 +54,25 @@ def get_cosine_sim(tfidf_matrix):
 
 
 with Session.begin() as session:
+    ps = PorterStemmer()
     df = pd.read_sql(session.query(Book).filter(Book.description != None).statement, session.bind)
+
+    # update numpy index in database
     for index, row in df.iterrows():
         db_id = row['id']
         book = session.query(Book).filter(Book.id == db_id).first()
         if book:
             book.np_id = index
             session.add(book)
+
+    for idx, row in df.iterrows():
+        cleaned_description = []
+        for word in word_tokenize(row['description']):
+            # if word not in stop_words:
+            cleaned_description.append(ps.stem(word))
+        cleaned_description = " ".join(cleaned_description)
+        df.loc[idx, 'description'] = cleaned_description
+
 
     description = df['description']
 
@@ -71,4 +93,4 @@ with Session.begin() as session:
     # get_cosine_sim(tfidf_matrix)
     # print(cosine_sim_matrix)
     # print(type(cosine_sim_matrix))
-    np.save('cosine_sim_matrix', cosine_sim_matrix)
+    np.save('cosine_sim_matrix_stemmed', cosine_sim_matrix)

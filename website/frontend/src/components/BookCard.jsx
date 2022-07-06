084f8facaebc3ee86abc88e388a6602ae1cec7e4
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-06 18:46:29 +0800

    fix: broken link to individual book
    and remove goodreads link in card because anchor tags cannot be nested

--- website/frontend/src/components/BookCard.jsx
+++ website/frontend/src/components/BookCard.jsx
@@ -1,13 +1,12 @@
 export function BookCard({ id, link, title, description, authors }) {
   return (
-    <a href={id}>
+    <a href={`/book/${id}`}>
       <h2>{title}</h2>
       <ul>
         {authors.map(author => (
           <li key={author.link}>{author.name}</li>
         ))}
       </ul>
-      <a href={link}>Goodreads Link: {link}</a>
       <p>Description: {description}</p>
     </a>
   );

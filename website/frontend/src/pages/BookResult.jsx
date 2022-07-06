9bc2189d231e3b7d84a1cf8d9b03782b040d50d1
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-06 18:00:44 +0800

    feat: add page for individual book
    show book metadata and similar books

---
+++ website/frontend/src/pages/BookResult.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { BookCard } from '../components';
+import bookService from '../services/books';
+
+export function BookResult() {
+  const { id } = useParams(); // get bookID from url parameter
+  const [currentBook, setCurrentBook] = useState(null);
+  const [similarBooks, setSimilarBooks] = useState(null);
+
+  useEffect(() => {
+    const setBookData = async id => {
+      const newCurrBook = await bookService.getBook(id);
+      const newSimilarBooks = await bookService.getSimilarBooks(id);
+      setCurrentBook(newCurrBook);
+      setSimilarBooks(newSimilarBooks);
+    };
+    setBookData(id);
+  }, [id]);
+
+  if (currentBook) {
+    return (
+      <div>
+        <h1>{currentBook.title}</h1>
+        <ul>
+          {currentBook.authors.map(author => (
+            <li key={author.link}>{author.name}</li>
+          ))}
+        </ul>
+        <p>Goodreads Link: {currentBook.link}</p>
+        <p>Description: {currentBook.description}</p>
+        <h2>Similar Books</h2>
+        {similarBooks.length > 0 &&
+          similarBooks.map(result => (
+            <BookCard
+              key={result.id}
+              link={result.link}
+              title={result.title}
+              description={result.description}
+              authors={result.authors}
+            />
+          ))}
+      </div>
+    );
+  }
+}

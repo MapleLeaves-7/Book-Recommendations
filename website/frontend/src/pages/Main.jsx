2a68c507afa995fc1bf0cff2103db777a6b7a706
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-06 17:58:26 +0800

    feat: add routes
    one route to main page and one route for individual book
    move logic for main page into separate file

---
+++ website/frontend/src/pages/Main.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { MeiliSearch } from 'meilisearch';
+import { BookCard } from '../components';
+
+const client = new MeiliSearch({
+  host: 'http://127.0.0.1:7700',
+  // apiKey: process.env.MEILI_FRONTEND_API_KEY,
+});
+
+const index = client.index('books');
+
+export function Main() {
+  const [searchedWord, setSearch] = useState('');
+  const [searchResults, setResults] = useState([]);
+
+  useEffect(() => {
+    // create and call scoped async function
+    (async function () {
+      const search = await index.search(searchedWord);
+      setResults(search.hits);
+    })();
+  }, [searchedWord]);
+
+  return (
+    <div>
+      <input
+        name="Search"
+        type="text"
+        value={searchedWord}
+        onChange={({ target }) => setSearch(target.value)}
+      />
+      <div>
+        <h1>searched results</h1>
+        {searchResults.map(result => (
+          <BookCard
+            key={result.id}
+            link={result.link}
+            title={result.title}
+            description={result.description}
+            authors={result.authors}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}

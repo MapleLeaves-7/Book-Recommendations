b52c501770b27ad4814894f903410e03ff00d825
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-06 18:48:46 +0800

    chore: add link back to home page

--- website/frontend/src/App.js
+++ website/frontend/src/App.js
@@ -1,10 +1,13 @@
 import { Main, BookResult } from './pages';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Router>
+        <nav>
+          <Link to="/">SimilarBookSearch</Link>
+        </nav>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/book/:id" element={<BookResult />} />

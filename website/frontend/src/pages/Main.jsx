71f13670a086a2ff256143a5c756db6079cca91a
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-12 17:47:57 +0800

    refactor: move services related to search to separate file
    more organised -> search service is in separate file to be accessed from anywhere

--- website/frontend/src/pages/Main.jsx
+++ website/frontend/src/pages/Main.jsx
@@ -1,13 +1,8 @@
 import { useEffect, useState } from 'react';
-import { MeiliSearch } from 'meilisearch';
 import { BookCard } from '../components';
 
-const client = new MeiliSearch({
-  host: 'http://127.0.0.1:7700',
-  // apiKey: process.env.MEILI_FRONTEND_API_KEY,
-});
-
-const index = client.index('books');
+import searchService from '../services/search';
+const { index } = searchService;
 
 export function Main() {
   const [searchedWord, setSearch] = useState('');

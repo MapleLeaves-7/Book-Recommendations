4c04201af75c74a285f286a2709d39dc35f187c6
Grace <intern.grace@intelllex.com>
2022-07-18 18:03:44 +0800

    chore: add arvo and titillium web as custom css font

--- website/frontend/tailwind.config.js
+++ website/frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        arvo: ['Arvo', 'sans-serif'],
+        titillium: ['Titillium Web', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };

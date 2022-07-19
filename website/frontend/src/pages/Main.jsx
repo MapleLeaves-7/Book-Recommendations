2ed97cbac0bbba3de1833379bb809e6b0805b7d9
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-19 17:11:35 +0800

    feat: style search bar on main page

--- website/frontend/src/pages/Main.jsx
+++ website/frontend/src/pages/Main.jsx
@@ -12,15 +12,27 @@ export function Main() {
 
   return (
     <div>
-      <form onSubmit={submitSearch}>
-        <input
-          name="Search"
-          type="text"
-          value={searchedWord}
-          onChange={({ target }) => setSearch(target.value)}
-        />
-        <button type="submit">search</button>
-      </form>
+      <div className="flex flex-col items-center w-screen gap-3 py-4 font-arvo">
+        <h1 className="text-xl font-bold ">Find books similar to...</h1>
+        <form
+          onSubmit={submitSearch}
+          className="flex flex-col items-center w-full gap-4"
+        >
+          <input
+            name="Search"
+            type="text"
+            value={searchedWord}
+            onChange={({ target }) => setSearch(target.value)}
+            className="w-10/12 px-3 py-1 border-transparent rounded-lg shadow-search"
+          />
+          <button
+            type="submit"
+            className="w-32 py-1 text-white rounded-md shadow-button bg-button"
+          >
+            Search
+          </button>
+        </form>
+      </div>
     </div>
   );
 }

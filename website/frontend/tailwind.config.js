c8b600994515a1be0a9328c8d60e2933da23c44a
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-19 17:10:28 +0800

    chore: add custom box shadows

--- website/frontend/tailwind.config.js
+++ website/frontend/tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
         'off-white': '#FEFEFE',
         button: '#A5A58D',
       },
+      boxShadow: {
+        button: '2px 2px 2px rgba(0, 0, 0, 0.1)',
+        'main-card': '3px 4px 5px rgba(0, 0, 0, 0.20)',
+        'sub-card': '0px 4px 15px rgba(0, 0, 0, 0.20)',
+        search: '0px 3px 5px 3px rgba(0, 0, 0, 0.20)',
+      },
     },
   },
   plugins: [],

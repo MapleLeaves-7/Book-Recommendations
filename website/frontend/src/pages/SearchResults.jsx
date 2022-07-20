866d68a05c03eec57684a5c4c3013b50526b4e23
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-20 15:41:00 +0800

    feat: style search results page

--- website/frontend/src/pages/SearchResults.jsx
+++ website/frontend/src/pages/SearchResults.jsx
@@ -30,34 +30,38 @@ export function SearchResults() {
   };
 
   return (
-    <div>
-      <form
-        onSubmit={submitSearch}
-        className="flex flex-col items-center w-full gap-4"
-      >
-        <input
-          name="Search"
-          type="text"
-          value={searchedWord}
-          onChange={({ target }) => setSearch(target.value)}
-          className="w-10/12 search-bar"
-        />
-        <button type="submit" className="search-button">
-          Search
-        </button>
-      </form>
-
-      <div>
-        <h1>searched results</h1>
-        {searchResults.map(result => (
-          <BookCard
-            key={result.id}
-            id={result.id}
-            title={result.title}
-            authors={result.authors}
-            img={result.book_cover}
+    <div className="flex justify-center">
+      <div className="flex flex-col items-center w-10/12 max-w-5xl ">
+        <form
+          onSubmit={submitSearch}
+          className="flex items-center w-full gap-4 mt-8 mb-8"
+        >
+          <input
+            name="Search"
+            type="text"
+            value={searchedWord}
+            onChange={({ target }) => setSearch(target.value)}
+            className="w-10/12 search-bar"
           />
-        ))}
+          <button type="submit" className="search-button">
+            Search
+          </button>
+        </form>
+
+        <div className="w-full">
+          <h2 className="ml-4 text-lg font-bold font-arvo">Search results</h2>
+          <div className="grid grid-cols-fit">
+            {searchResults.map(result => (
+              <BookCard
+                key={result.id}
+                id={result.id}
+                title={result.title}
+                authors={result.authors}
+                img={result.book_cover}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );

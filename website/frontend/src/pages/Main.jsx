eda0cc2344e38c4eefa19a84ef10552b4bfa0eba
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-20 16:31:01 +0800

    chore: add max width for content container

--- website/frontend/src/pages/Main.jsx
+++ website/frontend/src/pages/Main.jsx
@@ -1,8 +1,8 @@
 import { SearchBar } from '../components';
 export function Main() {
   return (
-    <div>
-      <div className="flex flex-col items-center w-screen gap-3 py-4 font-arvo">
+    <div className="flex justify-center">
+      <div className="flex flex-col items-center w-screen gap-3 py-4 font-arvo container-lg">
         <h1 className="text-xl font-bold ">Find books similar to...</h1>
         <SearchBar />
       </div>

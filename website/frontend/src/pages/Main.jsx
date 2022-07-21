517dfdf7be6a6cc6e38af382ea5169b63e1dfb7d
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-21 16:40:21 +0800

    chore: standardise padding using container class

--- website/frontend/src/pages/Main.jsx
+++ website/frontend/src/pages/Main.jsx
@@ -2,7 +2,7 @@ import { SearchBar } from '../components';
 export function Main() {
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col items-center w-screen gap-3 py-4 font-arvo container-lg">
+      <div className="flex flex-col items-center w-screen gap-3 font-arvo container-lg">
         <h1 className="text-xl font-bold ">Find books similar to...</h1>
         <SearchBar />
       </div>

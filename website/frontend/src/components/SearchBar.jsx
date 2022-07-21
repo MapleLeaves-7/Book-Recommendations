2870a855862b348404c9b7191b752fac2300e1f7
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-21 19:21:41 +0800

    feat: add navigation for autosuggestion using arrow keys

--- website/frontend/src/components/SearchBar.jsx
+++ website/frontend/src/components/SearchBar.jsx
@@ -5,15 +5,64 @@ import searchService from '../services/search';
 const { index } = searchService;
 
 export function SearchBar() {
+  const [active, setActive] = useState(null);
   const [results, setResults] = useState([]);
   const [searchedWord, setSearch] = useState('');
   const [hasMoreResults, setHasMoreResults] = useState(false);
 
   const navigate = useNavigate();
-  const submitSearch = event => {
+
+  const submitSearch = () => {
+    navigate(`/search`, { state: searchedWord });
+  };
+
+  const handleEnter = event => {
     event.preventDefault();
 
-    navigate(`/search`, { state: searchedWord });
+    if (active >= 0 && active < results.length) {
+      let bookID = results[active].id;
+      return navigate(`/book/${bookID}`);
+    }
+    submitSearch();
+  };
+
+  const onKeyDown = e => {
+    // up arrow
+    if (e.keyCode === 38) {
+      // if already at the first option, set to null
+      if (active === 0) {
+        return setActive(null);
+      }
+      // if at last "show more..." option, then set to last book results option
+      if (active === -1) {
+        return setActive(results.length - 1);
+      }
+      // otherwise set to previous book result option
+      return setActive(active - 1);
+    }
+
+    // down arrow
+    if (e.keyCode === 40) {
+      if (active === null) {
+        // go to the first option
+        return setActive(0);
+      }
+      // at last book result option
+      if (active + 1 === results.length) {
+        // go to "show more..." option if it exists
+        if (hasMoreResults) {
+          return setActive(-1); // index -1 means that the "show more..." option is active
+        }
+        // at last book result option with no "show more..."
+        return setActive(null);
+      }
+      // at the last "show more..."
+      if (active === -1) {
+        return setActive(null);
+      }
+      // go to the next option
+      return setActive(active + 1);
+    }
   };
 
   const handleSearchChange = ({ target }) => {
@@ -32,24 +81,43 @@ export function SearchBar() {
   };
 
   const renderAutocomplete = () => {
+    let showAllClassName = 'cursor-pointer';
+    let activeClass = ' bg-gray-200';
     if (searchedWord) {
       if (results.length > 0) {
         return (
           <ul className="absolute w-full bg-off-white">
-            {results.map(result => (
-              <li key={result.id}>
-                <Link to={`/book/${result.id}`} className="flex gap-5">
-                  <img
-                    src={result.book_cover}
-                    alt={result.title}
-                    className="w-24 h-36"
-                  />
-                  <span className="flex-wrap">{result.title}</span>
-                </Link>
-              </li>
-            ))}
+            {results.map((result, index) => {
+              let resultClassName = 'flex gap-5';
+              return (
+                <li key={result.id}>
+                  <Link
+                    to={`/book/${result.id}`}
+                    className={
+                      index === active
+                        ? resultClassName + activeClass
+                        : resultClassName
+                    }
+                  >
+                    <img
+                      src={result.book_cover}
+                      alt={result.title}
+                      className="w-24 h-36"
+                    />
+                    <span className="flex-wrap">{result.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
             {hasMoreResults ? (
-              <li onClick={submitSearch} className="cursor-pointer">
+              <li
+                onClick={submitSearch}
+                className={
+                  active === -1
+                    ? showAllClassName + activeClass
+                    : showAllClassName
+                }
+              >
                 ...show all results
               </li>
             ) : (
@@ -63,7 +131,7 @@ export function SearchBar() {
 
   return (
     <form
-      onSubmit={submitSearch}
+      onSubmit={handleEnter}
       className="flex flex-col items-center w-full gap-4"
     >
       <div className="relative w-10/12">
@@ -73,6 +141,7 @@ export function SearchBar() {
           value={searchedWord}
           onChange={handleSearchChange}
           onFocus={handleSearchChange}
+          onKeyDown={onKeyDown}
           className="w-full search-bar"
         />
         {renderAutocomplete()}

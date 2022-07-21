7ed9470e56ccde30a282fc6d216842145cfc1cf5
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-21 15:11:05 +0800

    feat: style individual book page and add toggle for description

---
+++ website/frontend/src/utils/book.js
@@ -0,0 +1,21 @@
+const getAuthorDisplay = authors => {
+  let authors_display = '';
+  for (let author of authors) {
+    authors_display += `${author.name}, `;
+  }
+  authors_display = authors_display.slice(0, authors_display.length - 2);
+  return authors_display;
+};
+
+const getShortDescription = description => {
+  let words = description.split(' ');
+  if (words < 100) {
+    return description;
+  }
+
+  return words.slice(0, 97).join(' ') + '...';
+};
+
+const bookUtils = { getAuthorDisplay, getShortDescription };
+
+export default bookUtils;

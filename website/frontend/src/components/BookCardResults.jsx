f8e7f49ec9c97d4cba792afd46b4c297bb79119e
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-21 16:22:31 +0800

    feat: add option to have message when no results shown

--- website/frontend/src/components/BookCardResults.jsx
+++ website/frontend/src/components/BookCardResults.jsx
@@ -1,19 +1,23 @@
 import { BookCard } from '.';
 
-export function BookCardResults({ title, books }) {
+export function BookCardResults({ title, books, noResultsMessage }) {
   return (
     <div className="w-full">
       <h2 className="pb-3">{title}</h2>
       <div className="grid gap-4 grid-cols-fit">
-        {books.map(result => (
-          <BookCard
-            key={result.id}
-            id={result.id}
-            title={result.title}
-            authors={result.authors}
-            img={result.book_cover}
-          />
-        ))}
+        {books.length > 1 ? (
+          books.map(result => (
+            <BookCard
+              key={result.id}
+              id={result.id}
+              title={result.title}
+              authors={result.authors}
+              img={result.book_cover}
+            />
+          ))
+        ) : (
+          <div className="font-arvo">{noResultsMessage}</div>
+        )}
       </div>
     </div>
   );

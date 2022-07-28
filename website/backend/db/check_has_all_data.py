8900a83e43a7162634a6d1c9b89425037eae62af
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-07-28 11:37:14 +0800

    chore: copy db folder into backend

---
+++ website/backend/db/check_has_all_data.py
@@ -0,0 +1,37 @@
+from models import Book, get_engine
+from sqlalchemy import and_
+from sqlalchemy.orm import sessionmaker
+
+engine = get_engine()
+Session = sessionmaker(bind=engine)
+
+
+def check_has_all_data():
+    """
+    Check whether has all data field is labelled correctly
+    """
+    with Session.begin() as session:
+        books = session.query(Book).filter(and_(Book.has_all_data == False, Book.has_author == True, Book.has_genre == True, Book.has_setting == True,
+                                                Book.has_related_books == True, Book.title != None, Book.description != None, Book.num_pages != None,
+                                                Book.num_ratings != None, Book.rating_value != None, Book.date_published != None))
+        count = 0
+        for book in books:
+            count += 1
+            print(book.title)
+        print(count)
+
+
+def update_has_all_data():
+    """
+    Update has all data field by checking whether the relevant columns are null
+    """
+    with Session.begin() as session:
+        books = session.query(Book).filter(and_(Book.has_all_data == False, Book.has_author == True, Book.has_genre == True, Book.has_setting == True,
+                                                Book.has_related_books == True, Book.title != None, Book.description != None, Book.num_pages != None,
+                                                Book.num_ratings != None, Book.rating_value != None, Book.date_published != None))
+        for book in books:
+            book.has_all_data = True
+
+
+update_has_all_data()
+check_has_all_data()
---
+++ website/backend/db/models.py
@@ -0,0 +1,175 @@
+import sys
+from pathlib import Path
+
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy import (
+    Integer, String, Date, Float, Boolean
+)
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import UniqueConstraint, create_engine, Column, Table, ForeignKey
+
+# add parent directory to path so that database settings can always be imported properly
+parent_dir = Path(__file__).parents[1]
+sys.path.insert(0, str(parent_dir))
+
+from db.db_config import postgresql as db_settings  # nopep8 (disable autopep8 formatting for this line) -> so that import statement does not get moved to the top
+
+
+Base = declarative_base()
+
+
+def get_engine():
+    user = db_settings["user"]
+    password = db_settings["password"]
+    host = db_settings["host"]
+    port = db_settings["port"]
+    db = db_settings["db"]
+
+    url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+    if not database_exists(url):
+        create_database(url)
+    return create_engine(url)
+
+
+def create_all_tables(engine):
+    Base.metadata.create_all(engine)
+
+
+def drop_all_tables(engine):
+    Base.metadata.drop_all(engine)
+
+
+# Association table for many to many relationship between books and authors
+# https://docs.sqlalchemy.org/en/13/orm/basic_relationships.html#many-to-many
+book_author = Table('book_author',
+                    Base.metadata,
+                    Column('id', Integer, primary_key=True),
+                    Column('book_id', Integer, ForeignKey('books.id')),
+                    Column('author_id', Integer, ForeignKey('authors.id')))
+
+# Association table for many to many relationship between books and genres
+book_genre = Table('book_genre',
+                   Base.metadata,
+                   Column('id', Integer, primary_key=True),
+                   Column('book_id', Integer, ForeignKey('books.id')),
+                   Column('genre_id', Integer, ForeignKey('genres.id')))
+
+# Association table for many to many relationship between books and story settings
+book_story_setting = Table('book_story_setting',
+                           Base.metadata,
+                           Column('id', Integer, primary_key=True),
+                           Column('book_id', Integer, ForeignKey('books.id')),
+                           Column('story_settings', Integer, ForeignKey('story_settings.id')))
+
+# Association table for many to many relationship between books and related books -> related books are crawled from goodreads
+book_related_book = Table('book_related_book',
+                          Base.metadata,
+                          Column('id', Integer, primary_key=True),
+                          Column('book_id', Integer, ForeignKey('books.id')),
+                          Column('related_book_id', Integer, ForeignKey('books.id')),
+                          UniqueConstraint('book_id', 'related_book_id', name='unique_related_books')
+                          )
+
+
+class BookSimilarBook(Base):
+    # similar books are based on own recommendations
+    __tablename__ = "book_similar_book"
+
+    id = Column(Integer, primary_key=True)
+    current_book_id = Column(Integer, ForeignKey('books.id'))
+    similar_book_id = Column(Integer, ForeignKey('books.id'))
+    sim = Column(Float)  # cosine similarity bewteen books
+
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    link = Column(String, nullable=False, unique=True)
+    # note: has_all_data is false if any of the fields are null, including author, genre, setting and related books
+    has_all_data = Column(Boolean, nullable=False)
+    has_author = Column(Boolean)
+    has_genre = Column(Boolean)
+    has_setting = Column(Boolean)
+    has_related_books = Column(Boolean)
+    title = Column(String(250))
+    # M-to-M relationship between books and authors
+    authors = relationship('Author', secondary='book_author', backref='Book')
+    description = Column(String)
+    num_pages = Column(Integer)
+    num_ratings = Column(Integer)
+    rating_value = Column(Float(5))
+    date_published = Column(Date)
+    book_cover = Column(String)
+    language = Column(String(50))
+    # M-to-M relationship between books and genres
+    genres = relationship('Genre', secondary='book_genre', backref='Book')
+    # M-to-M relationship between books and story settings
+    settings = relationship(
+        'StorySetting', secondary='book_story_setting', backref='Book')
+    # M-to-M relationship between books and related books (this is a self-referential relationship) -> related books are crawled from goodreads
+    related_books = relationship('Book', secondary=book_related_book,
+                                 primaryjoin=id == book_related_book.c.book_id,
+                                 secondaryjoin=id == book_related_book.c.related_book_id)
+    # panda id when the data was first read and processed -> maps to respective row / column in numpy cosine similarity matrix that was dumped out
+    np_id = Column(Integer)
+
+    # note: similar books are based on own recommendations
+    # contains a list of BookSimilarBook objects where the id of the current book is the same as the current_book_id of BookSimilarBook
+    similar_books = relationship('BookSimilarBook', primaryjoin=id == BookSimilarBook.current_book_id)
+
+    def as_dict(self):
+        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+
+
+class Author(Base):
+    __tablename__ = "authors"
+
+    id = Column(Integer, primary_key=True)
+    link = Column(String, nullable=False, unique=True)
+    name = Column(String(50))
+
+    def as_dict(self):
+        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+
+
+class Genre(Base):
+    __tablename__ = "genres"
+
+    id = Column(Integer, primary_key=True)
+    link = Column(String, nullable=False, unique=True)
+    name = Column(String(50))
+
+    def as_dict(self):
+        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+
+
+class StorySetting(Base):
+    __tablename__ = "story_settings"
+
+    id = Column(Integer, primary_key=True)
+    link = Column(String, nullable=False, unique=True)
+    name = Column(String(50))
+
+    def as_dict(self):
+        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+
+
+# alternative relation
+# add the following 2 lines to the Book class
+# current_books = association_proxy("current_book_relations", "current_book")
+# similar_books = association_proxy("similar_books_relations", "similar_book")
+
+# class BookSimilarBook(Base):
+#     # similar books are based on own recommendations
+#     __tablename__ = "book_similar_book"
+
+#     id = Column(Integer, primary_key=True)
+#     current_book_id = Column(Integer, ForeignKey('books.id'))
+#     similar_book_id =  Column(Integer, ForeignKey('books.id'))
+#     sim = Column(Float) # cosine similarity bewteen books
+
+#     current_book = relationship(Book, primaryjoin=(current_book_id == Book.id), backref='similar_books_relations')
+#     similar_book = relationship(Book, primaryjoin=(similar_book_id == Book.id), backref='current_book_relations')

c721a3a9b5837aa5b61b2dfa665fe6f73eb7703e
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-08-02 12:44:14 +0800

    feat: add responsive design
    diff ui for search results page and individual book page

--- website/frontend/src/components/BookCardResults.jsx
+++ website/frontend/src/components/BookCardResults.jsx
@@ -1,10 +1,24 @@
 import { BookCard } from '.';
 
-export function BookCardResults({ title, books, noResultsMessage }) {
+export function BookCardResults({
+  title,
+  books,
+  noResultsMessage,
+  searchResultsPage,
+}) {
+  let bookCardClass = 'grid w-full gap-4 justify-items-center grid-cols-fit';
   return (
-    <div className="w-full">
-      <h2 className="pb-3">{title}</h2>
-      <div className="grid gap-4 grid-cols-fit">
+    <div className="flex flex-col items-center w-full">
+      <h2 className={searchResultsPage ? 'pb-3' : 'pb-3 sm:self-start'}>
+        {title}
+      </h2>
+      <div
+        className={
+          searchResultsPage
+            ? bookCardClass
+            : bookCardClass + ' sm:justify-items-start'
+        }
+      >
         {books.length > 0 ? (
           books.map(result => (
             <BookCard

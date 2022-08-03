df231f1a6a169f6de6b38b0750341df724e32900
MapleLeaves-7 <64523738+MapleLeaves-7@users.noreply.github.com>
2022-08-03 17:44:40 +0800

    feat: add section to show off similar books

--- website/frontend/src/pages/Main.jsx
+++ website/frontend/src/pages/Main.jsx
@@ -1,10 +1,70 @@
-import { SearchBar } from '../components';
+import { useEffect, useState } from 'react';
+import { BookCard, SearchBar } from '../components';
+
+import bookService from '../services/books';
+
+import { ReactComponent as RightArrowLogo } from '../images/right-arrow.svg';
+
 export function Main() {
+  const currentDisplayBookID = 196;
+  const [currentBook, setCurrentBook] = useState(null);
+  const [similarBooks, setSimilarBooks] = useState([]);
+  useEffect(() => {
+    (async function () {
+      const currentBook = await bookService.getBook(currentDisplayBookID);
+      const similarBooks = await bookService.getSimilarBooks(
+        currentDisplayBookID
+      );
+      setCurrentBook(currentBook);
+      setSimilarBooks(similarBooks.slice(0, 2)); // show only 2 similar books
+    })();
+  }, []);
+
   return (
     <div className="flex justify-center">
       <div className="flex flex-col items-center w-screen gap-3 font-arvo container-lg">
         <h1 className="text-xl font-bold ">Find books similar to...</h1>
         <SearchBar isMainPage={true} />
+
+        {/* Section to show off similar books */}
+        <div className="w-10/12 mt-6">
+          <h2 className="pb-3">For example:</h2>
+          <div className="flex justify-between px-10 pt-5 pb-8 rounded-md bg-light-orange">
+            {/* Section for current book */}
+            <div>
+              <h3 className="pb-2 text-lg">This book...</h3>
+              {currentBook !== null && (
+                <BookCard
+                  id={currentBook.id}
+                  title={currentBook.title}
+                  authors={currentBook.authors}
+                  img={currentBook.book_cover}
+                />
+              )}
+            </div>
+
+            <RightArrowLogo className="w-20 opacity-70" />
+            {/* Section for similar books */}
+            <div>
+              <h3 className="pb-2 text-lg">...is similar to these</h3>
+              <div className="flex justify-center gap-5">
+                {similarBooks.length > 0
+                  ? similarBooks.map(book => {
+                      console.log(book);
+                      return (
+                        <BookCard
+                          id={book.id}
+                          title={book.title}
+                          authors={book.authors}
+                          img={book.book_cover}
+                        />
+                      );
+                    })
+                  : null}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
